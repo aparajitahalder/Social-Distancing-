49f5c2f0c89fcc0c3ec1969a1b84c23662e80703
Gaurav bhojwani <www.gaurav10bhojwani@gmail.com>
2020-04-19 23:07:58 +0530

    Added code for image upload API in flask

---
+++ image_upload.py
@@ -0,0 +1,41 @@
+import os
+import urllib.request
+from werkzeug.utils import secure_filename
+from flask import Flask, request, redirect, jsonify
+
+UPLOAD_FOLDER = './UPLOADS'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/file-upload', methods=['POST'])
+def upload_file():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No image part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	if file.filename == '':
+		resp = jsonify({'message' : 'No image selected for uploading'})
+		resp.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		resp = jsonify({'message' : 'File successfully uploaded'})
+		resp.status_code = 201
+		return resp
+	else:
+		resp = jsonify({'message' : 'Allowed image types are png, jpg, jpeg'})
+		resp.status_code = 400
+		return resp
+
+if __name__ == "__main__":
+    app.run()
